44f962372b2901a2db8d3f2e14376cf6b1f93819
LindA370 <75022412+LindA370@users.noreply.github.com>
2021-02-16 14:33:34 +0200

    dapush klo 1433

--- php/laht.php
+++ php/laht.php
@@ -2,11 +2,54 @@
 $json=$_POST["fisu"];
 $fisu=json_decode($json);
 
-//$tall="Tallennus onnistui";
-//print $fisu;
+set_error_handler("anyError", E_ALL);
 
-print $fisu->laji."<br>";
-print $fisu->paino."<br>";
-print $fisu->email."<br>";
+
+print $fisu->name."<br>";
+print $fisu->rate."<br>";
 print $fisu->message."<br>";
+
+$name=name;
+$rate=rate;
+$message=message;
+
+print $nimi;
+
+
+$yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
+if (!$yhteys) {
+	$error=" Connection failed ";
+    print $error;
+    return;
+}
+$tietokanta=mysqli_select_db($yhteys, "ryhmä1_projekti");
+if (!$tietokanta) {
+	$error=" Connection failed1 ";
+    print $error;
+    return;
+}
+
+$sql="insert into ryhmä1_projekti(nimi, arvosana, palaute) values(?, ?, ?)";
+$stmt=mysqli_prepare($yhteys, $sql);
+if (!$stmt) {
+	$error=" SQL preparation failed ";
+    print $error;
+    return;
+}
+$ok=mysqli_stmt_bind_param($stmt, "ss" ,$name, $rate, $message);
+if (!$ok) {
+	$error=" SQL preparation failed ";
+    print $error;
+    return;
+}
+$ok=mysqli_stmt_execute($stmt);
+if (!$ok) {
+	$error=" Failed to save, please try again ";
+    print $error;
+    return;
+}
+mysqli_close($yhteys); 
+
+header("Location:££££.html");
+exit;
 ?>
